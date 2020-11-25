76fa373f824bb440bfdf280e5bf619afb14f350e
Mitiku Teshome <mitikutesh@gmail.com>
2020-11-25 16:12:58 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./util";
 function App() {
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
       <Song></Song>
